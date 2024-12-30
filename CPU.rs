ec6b60908ce82ab9f831497eccd393806bb6f25b
Kevin McKnight <kbm5985@psu.edu>
2024-12-29 22:21:00 -0500

    added LDA instrution

--- CPU.rs
+++ CPU.rs
@@ -9,16 +9,30 @@ pub struct CPU {
 impel CPU {
     pub fn new() -> Self {
         CPU{
-            register_a: 0,
+            register_a: 0,                                                     //initialize the registers
             register_x: 0,
             register_y: 0,
-            status: 0,
-            program_counter: 0,
+            status: 0,                                                         //initialize the ccr
+            program_counter: 0,                                                //initialize the program counter to point to memory addresses
         }
     }
 
     pub fn interpret(&mut self, program: Vec<u8>){
-        todo!("")
+        self.program_counter = 0;                                               //set program counter to 0
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        loop{
+
+            let opscode = program[self.program_counter as usize];               //gets the op code from where the program counter is pointing in memory
+            self.program_counter += 1;                                          //increase the program counter
+
+            match opscode{
+                0xA9 => {
+                    let param = program[]
+                }
+            }
+        }
     }
 
 }
